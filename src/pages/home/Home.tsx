e23a1e45e1d4fbcd08117a3cee798fdb5492a2f7
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 <abhijeetkumarlucknow@gmail.com>
2023-02-21 13:26:37 +0530

    card implimented

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,16 +5,19 @@ import DatailBanner from './components/DatailBanner';
 import CouseList from './components/CouseList';
 import ApplyBanner from './components/ApplyBanner';
 import Feedback from './components/Feedback';
+import UpcomingEvent from './components/UpcomingEvent';
 export default function Home() {
   return (
     <React.Fragment>
       <div className='bg-[#f0fdf8]'>
       <TopBanner />
       <DatailBanner />
+
       </div>
       <CouseList/>
       <ApplyBanner/>
       <Feedback/>
+      <UpcomingEvent/>
     </React.Fragment>
   )
 }
